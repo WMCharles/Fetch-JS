b661a5cbdb51bad24b61cd564204793a0edbee13
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-26 13:34:13 +0300

    add functions that allows user input and update the json file acting as db

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+//Form submit Event Listener
+document.getElementById('form').addEventListener('submit', userInput)
+
+// form that takes in user input 
+function userInput(e){
+    e.preventDefault();
+    let sheepObj = {
+        title:e.target.article_title.value,
+        image_url:e.target.url.value,
+        content:e.target.description.value
+    }
+    postSheep(sheepObj) //Up dates out json file using POST method
+    renderArticle(sheepObj) //To display it on our homepage
+}
+
+//This function is used to add items in the database
+function postSheep(sheepObj){
+    fetch("http://localhost:3000/articles", {
+        method:"POST",
+        headers: {
+            "content-type":"application/json",
+        },
+        body:JSON.stringify(sheepObj)
+    })
+    .then(res => res.json())
+    .then(sheep => console.log(sheep))
+}
+
 //The function that is used to fetch data from the json-server
 function fetchData(){
     fetch("http://localhost:3000/articles")
@@ -10,6 +38,7 @@ function renderArticle(data){
     let content = document.createElement('div')
     content.innerHTML = `
         <h3>${data.title}</h1>
+        <img class="img" src="${data.image_url}" />
         <p>${data.content}</p>
     `
     document.getElementById('content').appendChild(content)
